0aa3852d1fe101abc2fad80c730295176ba2801a
Klaes Sørensen <Klaesdrescher@gmail.com>
2024-06-25 12:33:47 +0200

    Update strong_con_comp.py

--- strong_con_comp.py
+++ strong_con_comp.py
@@ -1,115 +1,95 @@
-from dataclasses import dataclass, field
-
-@dataclass
-class Vertex:
-	""" Find all strongly connected components in a graph without having costs on the edges. """
-	name: str = field(hash=True)
-	neighbors: set['Vertex'] = field(default_factory=set) # default = []
-
-	def __str__(self):
-		return self.name
-
-	def __repr__(self) -> str:
-		return self.__str__()
-
-	def __eq__(self, o: object) -> bool:
-		return isinstance(o, Vertex) and self.name == o.name
-
-	def __hash__(self) -> int:
-		return hash(self.name)
-
-def get_all_connected_vertices(vertex: Vertex, connected_vertices: set | None = None) -> set[Vertex]:
-	if connected_vertices is None:
-		connected_vertices = set()
-	connected_vertices.add(vertex)
-	for neighbor in vertex.neighbors:
-		if neighbor not in connected_vertices:
-			connected_vertices |= get_all_connected_vertices(neighbor, connected_vertices)
-	return connected_vertices
-
-# get all vertices reachable from vertex
-def get_connections(graph: set[Vertex]) -> dict[Vertex, set[Vertex]]:
-	connections = {}
-	for vertex in graph:
-		connections[vertex] = get_all_connected_vertices(vertex)
-	return connections
-
-# get all strongly connected components in graph
-def get_strongly_connected_components(graph: set[Vertex]) -> list[set[Vertex]]:
-	connections = get_connections(graph)
-
-	components = []
-	for vertex in graph:
-		component = {vertex}
-		for other_vertex in graph:
-			if vertex is other_vertex:
-				continue
-
-			if vertex in connections[other_vertex] and other_vertex in connections[vertex]:
-				component.add(other_vertex)
-		components.append(component)
-	# remove duplicates
-	components = [component for i, component in enumerate(components) if component not in components[:i]]
-	return components
-
 import sys
-# Driver code
-if __name__ == '__main__':
-	# List of graph edges as per above diagram
-	a = Vertex('a')
-	b = Vertex('b')
-	c = Vertex('c')
-	d = Vertex('d')
-	e = Vertex('e')
-	f = Vertex('f')
-	g = Vertex('g')
-	h = Vertex('h')
-	i = Vertex('i')
-	j = Vertex('j')
+from collections import defaultdict
 
-	a.neighbors = {b}
-	b.neighbors = {f,c}
-	c.neighbors = {d}
-	d.neighbors = {e}
-	e.neighbors = {c}
-	f.neighbors = {i,e}
-	g.neighbors = {f}
-	h.neighbors = {j}
-	i.neighbors = {e}
-	j.neighbors = {b,g}
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = defaultdict(list)
+        self.Time = 0
+        self.node_index = {}
+        self.index_node = {}
 
-	graph = {a, b, c, d, e, f, g, h, i, j}
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
 
-	# CLI input syntax (optional): 
-	# nodes="<node_name>: <neighboor_1> <neighboor_2> ..., <node_name>: ..."
-	# Each node and its neighboors are comma separated, and each neighboor is space separated
-	# Works with nodes with no neighboors and so no ":" as well. 
-	# example: nodes="a: b c d, b: a d e, c, d: a e, e: a d, f: c e"
-	for arg in sys.argv:
-		if arg.startswith("nodes="):
-			splitOnComma = arg.split("=")[1].replace("\"", "").split(", ")
-			newGraph = set()
-			vertices = {}
-			for nodeNeighboorPair in splitOnComma:
-				if ":" in nodeNeighboorPair:
-					nodeNeighboorSplit = nodeNeighboorPair.split(": ")
-					node = nodeNeighboorSplit[0]
-					vertices[node] = Vertex(node)
-					neighbors = nodeNeighboorSplit[1]
-					if neighbors is not None:
-						for neighbor in neighbors.strip().split(" "):
-							if neighbor not in vertices:
-								vertices[neighbor] = Vertex(neighbor)
-							vertices[node].neighbors.add(vertices[neighbor])	
-					newGraph.add(vertices[node])
-				else:
-					vertices[nodeNeighboorPair] = Vertex(nodeNeighboorPair)
-					newGraph.add(vertices[nodeNeighboorPair])
+    def SCCUtil(self, u, low, disc, stack_member, stack):
+        disc[u] = self.Time
+        low[u] = self.Time
+        self.Time += 1
+        stack_member[u] = True
+        stack.append(u)
 
-			graph = newGraph
+        for v in self.graph[u]:
+            if disc[v] == -1:
+                self.SCCUtil(v, low, disc, stack_member, stack)
+                low[u] = min(low[u], low[v])
+            elif stack_member[v]:
+                low[u] = min(low[u], disc[v])
 
-	components = get_strongly_connected_components(graph)
+        w = -1
+        if low[u] == disc[u]:
+            while w != u:
+                w = stack.pop()
+                print(self.index_node[w], end=" ")
+                stack_member[w] = False
+            print("")
 
-	print('strongly connected components:')
-	for component in components:
-		print(component)
+    def SCC(self):
+        disc = [-1] * self.V
+        low = [-1] * self.V
+        stack_member = [False] * self.V
+        stack = []
+
+        for i in range(self.V):
+            if disc[i] == -1:
+                self.SCCUtil(i, low, disc, stack_member, stack)
+
+def parse_input(input_str):
+    nodes_str = input_str.strip().replace("\"", "").split(",")
+    graph = defaultdict(list)
+    nodes = set()
+
+    for nodeNeighboorPair in nodes_str:
+        if ":" in nodeNeighboorPair:
+            node, edges = nodeNeighboorPair.split(":")
+            node = node.strip()
+            nodes.add(node)
+            edges = edges.strip().split(" ")
+            for edge in edges:
+                if edge:
+                    graph[node].append(edge)
+                    nodes.add(edge)
+        else:
+            node = nodeNeighboorPair.strip()
+            nodes.add(node)
+            graph[node] = []
+
+    node_list = list(nodes)
+    node_index = {node: i for i, node in enumerate(node_list)}
+    index_node = {i: node for i, node in enumerate(node_list)}
+
+    g = Graph(len(node_list))
+    g.node_index = node_index
+    g.index_node = index_node
+
+    for node in graph:
+        for neighbor in graph[node]:
+            g.add_edge(node_index[node], node_index[neighbor])
+
+    return g
+
+if __name__ == "__main__":
+    input_str = None
+    start_node = None
+
+    for arg in sys.argv:
+        if arg.startswith("nodes="):
+            input_str = arg.split("=")[1]
+        elif arg.startswith("start="):
+            start_node = arg.split("=")[1].strip()
+
+    if input_str:
+        g = parse_input(input_str)
+        print("Strongly Connected Components in the given graph:")
+        g.SCC()
+
