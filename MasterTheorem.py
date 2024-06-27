b2d165e3bc120aab4534bb6fc121ddade371d3ac
Klaes Sørensen <Klaesdrescher@gmail.com>
2024-06-27 15:45:49 +0200

    Update MasterTheorem.py

--- MasterTheorem.py
+++ MasterTheorem.py
@@ -1,9 +1,16 @@
 import math
+import sys
 
 
 def solve_master_theorem(a, b, k, i):
+    if a <= 0 or b <= 0:
+        raise ValueError("a and b must be greater than 0")
+    if a == 1 and b == 1:
+        raise ValueError("a and b cannot both be 1")
+
     p = math.log(a) / math.log(b)
     result = ""
+
     if float_equals(p, k):
         result += format_poly_log(k, i + 1)
     elif p < k:
@@ -17,8 +24,7 @@ def solve_master_theorem(a, b, k, i):
         result = "Arithmetic error"
 
     recurrence_text = f"T(n) = {a if a != 1 else ''}T(n{f'/{b}' if b != 1 else ''}) + Θ({format_poly_log(k, i)})"
-    print(f"Recurrence text (check if it matches what you put in):"
-          f"\n{recurrence_text}")
+    print(f"Recurrence text (check if it matches what you put in):\n{recurrence_text}")
     return result
 
 
@@ -31,7 +37,7 @@ def format_poly_log(k, i):
         result = ""
     elif k == 0 and i == 0:
         result = "1"
-    elif k == 0.5:  
+    elif k == 0.5:
         result = "sqrt(n)"
     elif k == 1:
         result = "n"
@@ -49,14 +55,14 @@ def format_poly_log(k, i):
 
     return result
 
-import sys
+
 if __name__ == "__main__":
     a, b, c, i = 0, 0, 0, 0
 
     # Optional CLI input syntax
     # --hasLog, there exists a log(n) term
     # Example input: python MasterTheorem.py a=2 b=2 c=1 --hasLog
-    if len(sys.argv) != 0:
+    if len(sys.argv) > 1:
         for arg in sys.argv:
             if arg.startswith("a="):
                 a = float(arg.split("=")[1].strip())
@@ -68,15 +74,14 @@ if __name__ == "__main__":
                 i = 1
     else:
         print("Format:"
-          "\nT(n) = aT(n/b) + Θ(n^c * (log n)^i)"
-          "\nOBS If the answer gives something like log^2(n) which does not match one of the cases, then it cannot be solved")
+              "\nT(n) = aT(n/b) + Θ(n^c * (log n)^i)"
+              "\nOBS If the answer gives something like log^2(n) which does not match one of the cases, then it cannot be solved")
 
         a = float(input("Enter a:\n"))
         b = float(input("Enter b:\n"))
         c = float(input("Enter c:\n"))
         i = float(input("Enter i:\n"))
 
-
+    print(f"Input values: a={a}, b={b}, c={c}, i={i}")
     solved = solve_master_theorem(a, b, c, i)
-    print(f"Solution:"
-          f"\nΘ({solved})")
+    print(f"Solution:\nΘ({solved})")
